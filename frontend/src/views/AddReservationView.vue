5dbe83f3b1aabd47cb29cbf619fe1fee94713485
Fenix2033 <Fenix203317@gmail.com>
2022-06-16 01:08:52 +0200

    Fix reservation

--- frontend/src/views/AddReservationView.vue
+++ frontend/src/views/AddReservationView.vue
@@ -1,27 +1,21 @@
 <template>
   <div v-if="reservationStore.isloading">create reservation ...</div>
   <div v-else>
-    <v-form v-model="form" lazy-validation ref="form">
-      <v-text-field
-          v-model="title"
-          label="Title"
-      ></v-text-field>
-      <v-text-field
-          v-model="text"
-          label="Text"
-      ></v-text-field>
-      <v-select
-          v-model="role"
-          :items="items"
-          item-text="Role"
-          label="Role"
-          persistent-hint
-          return-object
-          single-line
-      ></v-select>
-      <v-btn @click="addReservation()" color="primary">Create</v-btn>
+    <v-form v-model="formValid" lazy-validation ref="form">
+
+      <v-btn @click="addReservation()" color="primary">Add</v-btn>
     </v-form>
   </div>
+
+  <div>
+    <v-select
+        v-model:="flightInput.flightId"
+        :items="flightsArray"
+        item-value="id"
+        item-text="title"
+        label="Flights"/>
+  </div>
+
 </template>
 
 <script>
@@ -29,6 +23,7 @@
 import {mapStores} from "pinia/dist/pinia";
 import {useReservationStore} from "../stores/ReservationStore";
 import Error from "../components/Error.vue";
+import {useFlightStore} from "../stores/FlightStore";
 
 export default {
   name: "Create",
@@ -40,26 +35,37 @@ export default {
   data() {
     return {
       formValid: true,
-      title: '',
-      text: ''
-    }
+      flightInput: {
+        flightId: 1,
+      },
+      flightsArray: [
+        {id: 1, title: "Mars", text: "Mars", time: "2033"},
+        {id: 2, title: "Venus", text: "Venus", time: "2034"},
+        {id: 3, title: "Neptun", text: "Neptun", time: "2035"},
+      ],
+    };
+  },
+
+  created() {
+    this.flightStore.loadAll()
   },
 
   computed: {
-    ...mapStores(useReservationStore),
+    ...mapStores(useReservationStore, useFlightStore),
   },
 
   methods: {
-    async addReservation(title, text) {
+
+    async addReservation(title, text, flight_id ) {
       await this.$refs.form.validate();
       if (!this.formValid) return;
-      await this.reservationStore.addReservation(this.title, this.text, this.fligts_id);
-
+      await this.reservationStore.addReservation(this.title, this.text, this.flight_id);
     },
   }
-}
+};
 
 </script>
 
-<style scoped>
+<style>
 </style>
+
